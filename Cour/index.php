1a22160d73b233e2f8fcc3eb17d0014f81404462
pxcleme <55398201+pxcleme@users.noreply.github.com>
2021-03-23 14:28:56 +0100

    Add files via upload

---
+++ Cour/index.php
@@ -0,0 +1,16 @@
+<?php
+echo"coucou";
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cour</title>
+</head>
+<body>
+    <p>boujour je suis dans l'html</p>
+</body>
+</html>
